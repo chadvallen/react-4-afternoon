1807cc6622928d7960246676cd0cb6e26c552253
Chad Allen <chadvallen@gmail.com>
2018-10-09 16:05:32 -0700

    Completed Afternoon Project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import App from './App';
+import { HashRouter as HR} from 'react-router-dom'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<HR>
+    <App />
+</HR>, document.getElementById('root'));
